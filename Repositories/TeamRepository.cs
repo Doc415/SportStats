b651998f67573ee732ca9ba052c1c90358a5c059
SERDAR SİPAHİOĞLU <94632226+Doc415@users.noreply.github.com>
2024-05-29 11:50:11 +0300

    team management update

--- Repositories/TeamRepository.cs
+++ Repositories/TeamRepository.cs
@@ -66,7 +66,7 @@ namespace SportStats.Repositories
         {
             try
             {
-                return await _context.Teams.ToListAsync();
+                return await _context.Teams.Include(x=> x.Players).ToListAsync();
             }
             catch(Exception ex) 
             {
